69e22ed4be77c7de324c918a6317e748e3e061a5
Mohit Jain <mohitjain9875@gmail.com>
2021-01-17 11:55:45 +0530

    Boilerplate

---
+++ android/app/src/main/kotlin/com/example/MyStore_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.MyStore_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MyStore',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('MyStore'),
+      ),
+      body: Center(
+        child: Text('Start adding Products'),
+      ),
+    );
+  }
+}

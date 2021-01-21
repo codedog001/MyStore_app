e18f1d7a1064ce560f76f1830f879c6c5538f13e
Mohit Jain <mohitjain9875@gmail.com>
2021-01-21 12:32:33 +0530

    Showing a dialog to confirm Dismissal of cart item

--- lib/widgets/cart_item.dart
+++ lib/widgets/cart_item.dart
@@ -35,6 +35,29 @@ class CartItem extends StatelessWidget {
         ),
       ),
       direction: DismissDirection.startToEnd,
+      confirmDismiss: (direction) {
+        return showDialog(
+          context: context,
+          builder: (ctx) => AlertDialog(
+            title: Text('Are you sure?'),
+            content: Text('Do you want to remove the item?'),
+            actions: [
+              FlatButton(
+                child: Text('No'),
+                onPressed: () {
+                  Navigator.of(context).pop(false);
+                },
+              ),
+              FlatButton(
+                child: Text('Yes'),
+                onPressed: () {
+                  Navigator.of(context).pop(true);
+                },
+              )
+            ],
+          ),
+        );
+      },
       onDismissed: (direction) {
         Provider.of<Cart>(context, listen: false).removeItem(productId);
       },
